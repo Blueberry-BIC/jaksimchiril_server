8af3a34767f0cafc21161c3abe811f0a8a778a67
SUB1NJ0 <sub2nnj5@gmail.com>
2024-02-18 02:02:50 +0900

    FEAT: change participate api after BIC merge

--- routes/subin.js
+++ routes/subin.js
@@ -32,61 +32,37 @@ connectDB.then((client)=>{
     })
   })
 
-  // chall id로 조회해서 챌린지 정보 가져오는 api
-  router.get('/challenge/:chall_id', async (req, res)=>{
-    console.log("get challenge by id")
-    let challId = req.params.chall_id
-    console.log(challId)
-
-    let result = await db.collection('activated_chall').findOne({_id: new ObjectId(challId)})
-    console.log(result)
-    let money = result.money
-
-    /*var chall_id = result._id.toString()
-    var challName = result.challName
-    var chall*/
-
-    res.json({
-      depositData: money
-    })
-  })
-
-  //참가하기 눌렀을 때 해당 challenge document에 user id 추가 및 user num 업데이트하는 api
-  router.patch('/challenge/:chall_id', async (req, res)=>{
-    console.log("put user_id to activated_chall")
+  // 참가하기 눌렀을 때 activated_chall collection 업데이트하는 api
+  router.put('/participate/:chall_id', async (req, res)=> {
+    console.log("PUT user_list")
     const challId = req.params.chall_id
     //console.log(req.body)
-    let result = await db.collection('activated_chall').findOne({_id: new ObjectId(challId)})
-    let userlist = result.user_list
-    let usernum = result.user_num
-    if (userlist.includes(req.body.data, 0)==false){
-      userlist.push(req.body.data)
-      usernum = userlist.length
+    let msg = ""
 
-      let result2 = await db.collection('activated_chall').updateOne({_id: new ObjectId(challId)}, {$set: {user_list: userlist, user_num: usernum}}, function(err, res){
-        if (err) {
-          throw err
-        }
-        console.log("document 수정 완료")
-        console.log(result2)
-        db.close()
-      })
+    var usernum = req.body.userNum
+    var userlist = req.body.userList
+
+    try {
+      let result = await db.collection('activated_chall').updateOne({_id: new ObjectId(challId)}, {$set: {user_list: userlist, user_num: usernum}})
+      msg = "챌린지 참여 완료되었습니다."
+    } catch (err) {
+      console.log(`ERROR: ${err}`)
     }
-    
-    //console.log(result)
+
     res.json({
-      data: "Patch Success"
+      data: msg
     })
 
   })
 
-  //참가하기 눌렀을 때 해당 user document에 challenge id 추가하는 api
-  router.patch('/user/:user_id', async (req, res)=>{
-    console.log("put challenge_id to user")
+  //참가하기 눌렀을 때 user collection 업데이트하는 api
+  router.patch('/participate/:user_id', async (req, res)=>{
+    console.log("PATCH progress_chall")
     const userId = req.params.user_id
     //console.log(req.body)
     let result = await db.collection('user').findOne({_id: new ObjectId(userId)})
     let progresschall = result.progress_chall
+    let msg = ""
 
     if (progresschall.includes(req.body.data, 0) == false){
       if (progresschall[0] == ""){
@@ -95,27 +71,24 @@ connectDB.then((client)=>{
       else {
         progresschall.push(req.body.data)
       }
-
-      let result2 = await db.collection('user').updateOne({_id: new ObjectId(userId)}, {$set: {progress_chall: progresschall}}, function(err, res){
-        if (err) {
-          throw err
-        }
-        console.log("document 수정 완료")
-        console.log(result2)
-        db.close()
-      })
+      try {
+        let result2 = await db.collection('user').updateOne({_id: new ObjectId(userId)}, {$set: {progress_chall: progresschall}})
+        msg = "챌린지 참여 완료되었습니다."
+      } catch (err) {
+        console.log(`ERROR: ${err}`)
+      }
     }
     else{
       console.log("이미 존재하는 챌린지")
+      msg = "이미 존재하는 챌린지입니다"
     }
 
     //console.log(progresschall)
     res.json({
-      data: "Patch Success"
+      data: msg
     })
   })
 
-  
 
 
 //이 파일 제일 하단에 router변수 export해줘야 server.js 메인파일에서 이 파일 접근가능
